ff87bf4acaff01b678e58b2ea3a85a32fbfe554e
alankrit1403 <91617798+alankrit1403@users.noreply.github.com>
2023-02-11 01:15:16 +0530

    revise

---
+++ stock buy&sell.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int max_profit=0 ;
+        int min_so_far=prices[0] ;
+        for(int i=0;i<prices.length;i++) {
+            min_so_far = Math.min(min_so_far,prices[i]) ;
+            int profit = prices[i]-min_so_far ;
+            max_profit = Math.max(max_profit,profit) ;
+        }
+        return max_profit ;
+    }
+}
